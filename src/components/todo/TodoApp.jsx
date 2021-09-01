ac30253d3b8cd1ac5aee56a4068c7be9def73cf9
Priyavrat Sharma <Priyavrat18@gmail.com>
2021-09-01 20:03:59 +0530

    Adding few Components

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,14 +1,128 @@
 import React, { Component } from "react";
 import "./TodoApp.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class TodoApp extends Component {
   render() {
     return (
       <div className="TodoApp">
-        <LoginComponent />
+        <Router>
+          <>
+            <HeaderComponent />
+            <Switch>
+              <Route path="/" exact component={LoginComponent} />
+              <Route path="/login" component={LoginComponent} />
+              <Route path="/welcome/:name" component={WelcomeComponent} />
+              <Route path="/todos" component={ListTodosComponent} />
+              <Route component={ErrorComponent} />
+            </Switch>
+            <FooterComponent />
+          </>
+        </Router>
+
+        {/*<LoginComponent />*/}
       </div>
     );
   }
+}
+
+class HeaderComponent extends Component {
+  render() {
+    return (
+      <div>
+        Header <hr />
+      </div>
+    );
+  }
+}
+
+class FooterComponent extends Component {
+  render() {
+    return (
+      <div>
+        <hr /> Footer
+      </div>
+    );
+  }
+}
+class WelcomeComponent extends Component {
+  render() {
+    return (
+      <div>
+        Welcome {this.props.match.params.name} . You can manage your todos{" "}
+        <Link to="/todos">here</Link>
+      </div>
+    );
+  }
+}
+class ListTodosComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          description: "Learn React",
+          done: false,
+          targetDate: new Date(),
+        },
+        {
+          id: 2,
+          description: "Learn Java",
+          done: false,
+          targetDate: new Date(),
+        },
+        {
+          id: 3,
+          description: "Learn to Self Control",
+          done: false,
+          targetDate: new Date(),
+        },
+        {
+          id: 4,
+          description: "Learn full-stack Development",
+          done: false,
+          targetDate: new Date(),
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>List Todos</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>description</th>
+              <th>Is Completed?</th>
+              <th>Target Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.todos.map((todo) => (
+              <tr>
+                <td>{todo.id}</td>
+                <td>{todo.description}</td>
+                <td>{todo.done.toString()}</td>
+                <td>{todo.targetDate.toString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+function ErrorComponent() {
+  return (
+    <div>
+      An Error occurred as the page you are trying to reach doesn't exists.{" "}
+    </div>
+  );
 }
 
 class LoginComponent extends Component {
@@ -54,9 +168,9 @@ class LoginComponent extends Component {
       this.state.username === "priyavrat" &&
       this.state.password === "password"
     ) {
-      console.log("Successful");
-      this.setState({ showSuccessMessage: true });
-      this.setState({ hasLoginFailed: false });
+      this.props.history.push(`/welcome/${this.state.username}`);
+      /*this.setState({ showSuccessMessage: true });
+      this.setState({ hasLoginFailed: false }); */
     } else {
       console.log("Failed");
       this.setState({ showSuccessMessage: false });
